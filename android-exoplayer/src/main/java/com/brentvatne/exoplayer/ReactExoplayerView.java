e4dd6b28a54e0d4a093c35fc9adfeb78ed1631d4
Chris Stranex <chris@stranex.com>
2021-01-26 13:49:59 +0200

    Update ReactExoplayerView.java
    Don't pause playback on audio focus loss

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -679,7 +679,7 @@ class ReactExoplayerView extends FrameLayout implements
         switch (focusChange) {
             case AudioManager.AUDIOFOCUS_LOSS:
                 eventEmitter.audioFocusChanged(false);
-                pausePlayback();
+                // pausePlayback();
                 audioManager.abandonAudioFocus(this);
                 break;
             case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
